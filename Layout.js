a01af1bd82897d545591fff20f37366f6a33fac2
abisummers <abi.summers@govirtuo.com>
2020-04-23 16:13:17 +0200

    add info

---
+++ Layout.js
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+// export default ({ children }) => (
+//   <div
+//     style={{
+//       backgroundColor: 'tomato',
+//     }}>
+//     {children}
+//   </div>
+// ) 
+
