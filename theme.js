34363c56ad6a92d18a4c347a57d6b39dff227c7c
abisummers <abi.summers@govirtuo.com>
2020-04-23 18:18:52 +0200

    styling

--- theme.js
+++ theme.js
@@ -1,11 +1,21 @@
 export default {
   styles: {
     a: {
-      color: 'white'
+      color: 'rgb(79, 80, 84)'
     },
     img: {
       width: 'auto',
       height: '300px'
-    }
-  }
+    }, 
+    ul: {
+      width: '50%'
+    },
+    h1: {
+      margin: '0'
+    }, 
+  },
+      colors: {
+        text: 'rgb(79, 80, 84)',
+        background: 'rgb(223, 235, 242)',
+      },
 }
